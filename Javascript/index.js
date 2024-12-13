1db656c4b176a7d40d27010c719ba53404e62a3a
Abdullah Irfan <hassanrashid0018@gmail.com>
2024-12-13 17:38:12 +0500

    New Content Added

--- Javascript/index.js
+++ Javascript/index.js
@@ -46,35 +46,45 @@ increaseBtn.addEventListener("click", () => {
 
 // Initialize the price on page load
 updatePrice();
-$(document).ready(function(){
-  $('.slider').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      responsive: [
-          {
-              breakpoint: 1024,
-              settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 1,
-              }
-          },
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1,
-              }
-          },
-          {
-              breakpoint: 576,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-              }
-          }
-      ]
+  $(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 0,
+      arrows:true,
+      nav: true,
+      dots: false,
+      autoplay: false,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 },
+        1200: { items: 4 },
+      },
+    });
+  });
+
+$(document).ready(function () {
+  $(".custom-carousel").owlCarousel({
+    items: 2,
+    margin: 15,
+    loop: true,
+    nav: true, // Enable navigation arrows
+    dots: true,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ], // Customize arrows (optional)
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1024: {
+        items: 2,
+      },
+    },
   });
 });
